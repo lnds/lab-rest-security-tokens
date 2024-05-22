ad71e48d0439d5b024e6f4afb3443d38383a8af0
Eduardo Díaz <ediaz@dcc8090.cl>
2024-05-22 13:37:58 -0400

    add user to sql migrations

---
+++ sql_migrations/V3__Add_users.sql
@@ -0,0 +1,3 @@
+insert into users(name,email,password) values('Alumno','alumno@uni.edu','$2b$10$Dvri.d6dAcMh2MURJu9gQOrWqAjmEIDhCOlPKDB5hAwqM2D6/Hh9a');
+insert into users(name,email,password) values('Profesor','profe@uni.edu','$2b$10$7lcwftcazMdoIHYfFoUq9uGBL.njVqpGawefczZSbZ6Qn3tzcXKvW');
+insert into users(name,email,password) values('Usuario', 'usuario@a.com', '$2b$10$rkcRG6OhJupIulg37OnVo.FM.CH2z.w1an1fiHAsZ92/AEm2Lo8C.');
